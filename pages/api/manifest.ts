7a833d65781b6e18c11e4424a552f967b788c523
godspeed7 <pawelpikus7@gmail.com>
2022-05-18 18:53:40 +0200

    Add extensions to app manifest.

--- pages/api/manifest.ts
+++ pages/api/manifest.ts
@@ -1,36 +1,38 @@
 import { NextApiHandler } from "next";
 import { print } from "graphql/language/printer";
-import fg from 'fast-glob';
-import path from 'path';
+import fg from "fast-glob";
+import path from "path";
 
 import { version, name } from "../../package.json";
 import * as GeneratedGraphQL from "../../generated/graphql";
 import { getBaseURL } from "../../lib/middlewares";
 
-const capitalize = (value: string) => value.charAt(0).toUpperCase() + value.slice(1);
+const capitalize = (value: string) =>
+  value.charAt(0).toUpperCase() + value.slice(1);
 
 const inferWebhooks = async (baseURL: string) => {
-  const entries = await fg(['*.ts'], { cwd: 'pages/api/webhooks' });
+  const entries = await fg(["*.ts"], { cwd: "pages/api/webhooks" });
 
   const dropFileExtension = (filename: string) => path.parse(filename).name;
   const r = entries.map(dropFileExtension).map((name: string) => {
-
-    const camelcaseName = name.split('-').map(capitalize).join('');
+    const camelcaseName = name.split("-").map(capitalize).join("");
     const statement = `${camelcaseName}SubscriptionDocument`;
-    const query = statement in  GeneratedGraphQL ? print((GeneratedGraphQL as any)[statement]) : '';
+    const query =
+      statement in GeneratedGraphQL
+        ? print((GeneratedGraphQL as any)[statement])
+        : "";
 
     return {
       name,
       asyncEvents: [name.toUpperCase().replace("-", "_")],
-      query, 
+      query,
       targetUrl: `${baseURL}/api/webhooks/${name}`,
       isActive: true,
-    }
-  })
+    };
+  });
 
-  return r
-}
-
+  return r;
+};
 
 const handler: NextApiHandler = async (request, response) => {
   const baseURL = getBaseURL(request);
@@ -40,14 +42,23 @@ const handler: NextApiHandler = async (request, response) => {
   const manifest = {
     id: "saleor.app",
     version: version,
+    appUrl: "https://metal-experts-fix-159-255-181-88.loca.lt/extensions",
     name: name,
-    permissions: ["MANAGE_ORDERS"],
+    permissions: ["MANAGE_CHECKOUT"],
     configurationUrl: `${baseURL}/configuration`,
     tokenTargetUrl: `${baseURL}/api/register`,
     webhooks,
+    extensions: [
+      {
+        label: "Carts",
+        mount: "NAVIGATION_ORDERS",
+        target: "APP_PAGE",
+        url: "/extensions",
+      },
+    ],
   };
 
   response.json(manifest);
-}
+};
 
 export default handler;
---
+++ pages/extensions.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const extensions = (props: Props) => {
+  return <div>Get abandoned carts</div>;
+};
+
+export default extensions;
